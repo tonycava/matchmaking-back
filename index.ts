89bca474ac525e8ef0c0deaca976b9075916c7b3
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2023-03-13 12:06:41 +0100

    add: docker environment for the backend

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log('Hola que tal')
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+	res.send('Hello World! chang');
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port http://localhost:${PORT}`);
+});
