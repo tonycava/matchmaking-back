693813fdcb3d0396db5885e3b524296c023c3ded
tonycava <anthony.cavagne@ynov.com>
2023-05-16 08:38:54 +0200

    test: re deploy

--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -10,7 +10,7 @@ const router = express.Router();
 
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 2, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	max: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 	message: new ALMResult('Please wait before upload a new profile image', 429)
